d90028b998686e11d62087d340391fe4fa39bdbe
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-04-30 22:40:12 -0300

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e
 
 echo ">> Rodando migrações do Django..."
 python manage.py migrate --noinput
+
+echo ">> Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
 
 echo ">> Iniciando servidor Gunicorn..."
 gunicorn config.wsgi:application --bind 0.0.0.0:8080
