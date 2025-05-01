e15da34c27a9341af67d3168a482b7d60b337adb
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-04-30 21:43:42 -0300

    Adiciona STATICFILES_STORAGE para servir estáticos com Whitenoise

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,8 @@ USE_TZ = True
 # Arquivos estáticos
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Configuração padrão do DRF
 REST_FRAMEWORK = {
