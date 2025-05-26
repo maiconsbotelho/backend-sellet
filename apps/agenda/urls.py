7ccbd0c50ee5e3669cd349a8c682bbec53408ef9
maiconsbotelho <maicon.sbotelho@hotmail.com>
2025-05-26 19:38:30 -0300

    feat: criado api para mostrar expediente

--- apps/agenda/urls.py
+++ apps/agenda/urls.py
@@ -2,11 +2,13 @@ from rest_framework.routers import DefaultRouter
 from apps.agenda.views.expediente import HorarioExpedienteViewSet
 from apps.agenda.views.agendamento import AgendamentoViewSet
 from django.urls import path, include
+from apps.agenda.views.expediente import horarios_estabelecimento
 
 router = DefaultRouter()
 router.register(r'expediente', HorarioExpedienteViewSet, basename='expediente')
 router.register(r'agendamentos', AgendamentoViewSet, basename='agendamentos')
 
 urlpatterns = [
+    path('horarios-estabelecimento/', horarios_estabelecimento, name='horarios-estabelecimento'),
     path('', include(router.urls)),
 ]
